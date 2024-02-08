ffeeb2da8ef51f9c9247fd03a5cd4e367246df66
Binya.M.P <binyamp2024a@mca.ajce.in>
2024-02-08 09:59:43 +0530

    commit

--- RestaurantManagement/page1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Role,Person,Category,Menutbl,Subcategory
-# Register your models here.
-admin.site.register(Role)
-# Register your models here.
-admin.site.register(Person)
-admin.site.register(Category)
-admin.site.register(Menutbl)
-admin.site.register(Subcategory)
